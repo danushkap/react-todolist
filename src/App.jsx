d6707d5587467e8d91bf0209fb896e01894406f6
Danushka Padukka <danushka.padukka@gmail.com>
2022-10-20 12:57:59 +0530

    C07:
    * Layout
    * Css
    * Content - List, Check, Delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './Header';
+import ItemList from './ItemList';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
+      <ItemList />
+      <Footer />
     </div>
   )
 }
