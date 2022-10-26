893214af4c420f1fbc208666e1facaf403c3ef92
Danushka Padukka <danushka.padukka@gmail.com>
2022-10-26 12:00:21 +0530

    Remove apiRequest wrapper and its CRUD usages

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,8 @@ import ItemList from './ItemList';
 import EditItem from './EditItem';
 import About from './About';
 import Missing from './Missing';
-import apiRequest from './apiRequest';
 
 const App = () => {
-    const API_URL = 'http://localhost:3500/items'
 
     const [items, setItems] = useState([]);
     const [newItem, setNewItem] = useState('')
@@ -21,74 +19,17 @@ const App = () => {
 
     useEffect(() => {
         const getItems = async () => {
-            const apiOptions = {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-            const [response, err] = await apiRequest(API_URL, apiOptions)
-            if (err) {
-                setApiError(err)
-            }
-            else {
-                const listItems = await response.json()
-                setItems(listItems)
-                setApiError(null)
-                setIsLoading(false)
-            }
         }
         getItems();
     }, [])
 
     const addItem = async (item) => {
-        const id = items.length ? items[items.length - 1].id + 1 : 1;
-        const myNewItem = { id, checked: false, item };
-        const listItems = [...items, myNewItem];
-        setItems(listItems);
-
-        const apiOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(myNewItem)
-        }
-        const [_, err] = await apiRequest(API_URL, apiOptions);
-        if (err) setApiError(err);
     }
 
     const handleCheck = async (id) => {
-        const listItems = items.map((item) => {
-            return item.id === id
-                ? { ...item, checked: !item.checked }
-                : item
-        });
-        setItems(listItems);
-
-        const myItem = listItems.filter((item) => item.id === id)[0];
-        const apiOptions = {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ checked: myItem.checked })
-        };
-        const reqUrl = `${API_URL}/${id}`;
-        const [_, err] = await apiRequest(reqUrl, apiOptions);
-        if (err) setApiError(err);
     }
 
     const handleDelete = async (id) => {
-        const listItems = items.filter((item) => {
-            return item.id !== id
-        });
-        setItems(listItems);
-
-        const apiOptions = { method: 'DELETE' };
-        const reqUrl = `${API_URL}/${id}`;
-        const [_, err] = await apiRequest(reqUrl, apiOptions);
-        if (err) setApiError(err);
     }
 
     const handleSubmit = (e) => {
